04ab52a2d68fdc331afc7464ca856fad576b1cff
David R Lowry Lowry <ross@Davids-MacBook-Pro.local>
2017-05-31 02:23:21 -0400

    submission

--- models/task.model.js
+++ models/task.model.js
@@ -1,5 +1,5 @@
 'use strict';
-
+const Bluebird = require('bluebird')
 var db = require('./database');
 var Sequelize = require('sequelize');
 
@@ -20,13 +20,104 @@ var Task = db.define('Task', {
   due: Sequelize.DATE
 }, {
   //---------VVVV---------  your code below  ---------VVV----------
+  getterMethods: {
+  timeRemaining: function () {
+    if (!this.due) {
+      return Infinity;
+    } else {
+      return this.due - Date.now();
+    }
+  },
+  overdue: function() {
+    if (this.timeRemaining < 0 && (!this.complete)) {
+      return true
+    } else {
+      return false
+    }
+    }
+  },
+  instanceMethods: {
+    markComplete: function () {
+      // console.log('hi form mark complete')
+      return this.update({
+        complete: true
+      })
+    },
+    addChild: function (info) {
 
 
+      const nameHere = info.name
+      // console.log('namehere we go', name);
+      return Task.create({
+        name: nameHere,
+        parentId: this.id
+      })
 
-
-
-  //---------^^^---------  your code above  ---------^^^----------
+    },
+    getChildren: function () {
+      // console.log('this.parentId', this.parentId)
+      return Task.findAll({
+        include: [{
+          model: Task,
+          as: 'parent',
+          where: {parentId: this.parentId}
+        }]
+      })
+    //   .then(function(inst){
+    //     console.log('here it is yoiudflkajsdf;lkasdjf;lkadjf', inst)
+    //   })
+  },
+  getSiblings: function() {
+    return Task.findAll({
+      where: {
+        id: {
+          $ne: this.id
+        },
+        parentId: this.parentId
+      }
+    // include: [{
+    //     model: Task,
+    //     as: 'parent',
+    //     where: {parentId: this.parentId}
+    //   }],
+    })
+  }
+  },
+  classMethods: {
+    clearCompleted: function() {
+      return Task.destroy({
+        where: {
+          complete: true
+        }
+      })
+    },
+    completeAll: function () {
+      return this.update(
+        {complete: true},
+      {where: {complete: false}})
+                      // return Task.findAll({
+                      //   where: {
+                      //     complete: false
+                      //   }
+                      // })
+                      // .then(function(incompleteTasks){
+                      //   // console.log('incomplete', incompleteTasks)
+                      //   // const taskCompletePromise = incompletTask.update()
+                      //   // Promise.all()
+                      //   return Promise.all(incompleteTasks.map(aTask => {
+                      //     return aTask.markComplete()
+                      //   }))
+                      // })
+    }
+  }
 });
+
+
+
+
+// Task.hasMany(Task, {as: 'child'});
+  //---------^^^---------  your code above  ---------^^^----------
+
 
 Task.belongsTo(Task, {as: 'parent'});
 
@@ -34,5 +125,7 @@ Task.belongsTo(Task, {as: 'parent'});
 
 
 
-module.exports = Task;
 
+
+
+module.exports = Task;
